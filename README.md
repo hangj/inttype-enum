eb61cad2692d9bc3995b86970e58ab5e4cbff59b
hangj <guijie.han@gmail.com>
2023-06-26 18:35:17 +0800

    support default attribute

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # inttype-enum
+Converts an [`enum`] into an [`inttype`], and try to convert it back  
 
-Converts an [`enum`] into an [`inttype`], and try to convert it back
+
+Auto implement `From<enum> for inttype`, and `TryFrom<inttype> for enum`.
+if one(only one) variant is tagged with `#[default]`, then `From<inttype> for enum` will be implemented
+
 
 Usage example:  
 ```rs
@@ -15,4 +19,15 @@ enum Cmd {
 let conn: u8 = Cmd::Connect.into();
 assert!(matches!(Cmd::try_from(conn), Ok(Cmd::Connect)));
 assert!(matches!(Cmd::try_from(0), Err(_)));
+
+#[derive(IntType)]
+#[repr(u8)]
+enum Method {
+    A = 1,
+    B = 2,
+    #[default]
+    C = 3,
+}
+assert!(matches!(1.into(), Method::A));
+assert!(matches!(0.into(), Method::C));
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,25 @@
 //! let conn: u8 = Cmd::Connect.into();
 //! assert!(matches!(Cmd::try_from(conn), Ok(Cmd::Connect)));
 //! assert!(matches!(Cmd::try_from(0), Err(_)));
+//! 
+//! #[derive(IntType)]
+//! #[repr(u8)]
+//! enum Method {
+//!     A = 1,
+//!     B = 2,
+//!     #[default]
+//!     C = 3,
+//! }
+//! assert!(matches!(1.into(), Method::A));
+//! assert!(matches!(0.into(), Method::C));
 //! ```
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, ItemEnum, Meta, Type};
+use syn::{parse_macro_input, ItemEnum, Meta, Type, Result, Error, spanned::Spanned};
 use quote::quote;
 
 
-#[proc_macro_derive(IntType, attributes())]
+#[proc_macro_derive(IntType, attributes(default, ))]
 pub fn inttype(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as ItemEnum);
 
@@ -29,43 +40,83 @@ pub fn inttype(input: TokenStream) -> TokenStream {
         let Meta::List(ref meta_list) = attr.meta else {
             return None;
         };
-        let Some(ident) = meta_list.path.get_ident() else {
-            return None;
-        };
-        if ident.to_string().as_str() != "repr" {
+        if !attr.path().is_ident("repr") {
             return None;
         }
 
         syn::parse2::<Type>(meta_list.tokens.clone()).ok()
     }).expect("no repr(inttype) provided");
 
+    let mut default_var = None;
+    let mut result = None;
     let var = item.variants.iter().map(|v| {
+        if v.attrs.iter().find(|attr| {
+            attr.path().is_ident("default")
+        }).is_some() {
+            if default_var.is_some() {
+
+                result = Some(Error::new(v.span(), "Multiple default variables supplied! should be only one!")
+                        .into_compile_error());
+            }
+            default_var = Some(&v.ident);
+        }
+
+        if !matches!(v.fields, syn::Fields::Unit) {
+            result = Some(Error::new(v.span(), "every variant must be Unit kind, like `None`")
+                        .into_compile_error());
+        }
         &v.ident
     }).collect::<Vec<_>>();
 
-    quote! {
+    if let Some(ret) = result {
+        return ret.into();
+    }
+
+    let mut token_stream = quote! {
         impl From<#ident> for #ty {
             fn from(value: #ident) -> Self {
                 value as Self
             }
         }
+    };
 
-        impl TryFrom<#ty> for #ident {
-            type Error = io::Error;
-
-            fn try_from(value: #ty) -> Result<Self, Self::Error> {
-                #![allow(non_upper_case_globals)]
-                #(
-                    const #var: #ty = #ident::#var as #ty;
-                )*
-                match value {
-                    #( #var => Ok(Self::#var), )*
-                    _ => Err(Self::Error::new(
-                        io::ErrorKind::Unsupported, 
-                        format!("unsupported value: {value}"),
-                    ))
+    let from = if let Some(default_var) = default_var {
+        quote! {
+            impl From<#ty> for #ident {
+                fn from(value: #ty) -> Self {
+                    #![allow(non_upper_case_globals)]
+                    #(
+                        const #var: #ty = #ident::#var as #ty;
+                    )*
+                    match value {
+                        #( #var => Self::#var, )*
+                        _ => Self::#default_var,
+                    }
                 }
             }
         }
-    }.into()
+    } else {
+        quote! {
+            impl TryFrom<#ty> for #ident {
+                type Error = io::Error;
+    
+                fn try_from(value: #ty) -> Result<Self, Self::Error> {
+                    #![allow(non_upper_case_globals)]
+                    #(
+                        const #var: #ty = #ident::#var as #ty;
+                    )*
+                    match value {
+                        #( #var => Ok(Self::#var), )*
+                        _ => Err(Self::Error::new(
+                            io::ErrorKind::Unsupported, 
+                            format!("unsupported value: {value}"),
+                        ))
+                    }
+                }
+            }
+        }
+    };
+
+    token_stream.extend(from.into_iter());
+    token_stream.into()
 }
